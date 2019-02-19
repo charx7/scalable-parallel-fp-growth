88c7864fcaa936179adcb8eec03fbb49e7009424
charx <carlos.huertaso@udlap.mx>
2019-02-19 13:09:44 +0100

    initial commit

---
+++ pyspark_src/README.md
@@ -0,0 +1,9 @@
+# Example Package
+
+This is a simple example package. You can use
+[Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/)
+to write your content.
+
+To install under your local system use: "pip install -e ." the ".e" tag enables your source to be editable once you installed
+
+To generate a zipfile to be distributed to spark cluster use: "python setup.py sdist"     
---
+++ pyspark_src/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup , find_packages
+
+with open("README.md", "r") as fh:
+   long_description = fh.read()
+# long_description = "This is a long description of my pkg"
+
+setup(
+    name="pyspark-recom-engine",
+    version="0.1",
+    author="Charx, Michael, Ankit",
+    author_email="carlos.huertaso@udlap.mx",
+    description="Recommendation engine using spark",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="tbd",
+    packages=find_packages(),
+    install_requires=[
+        'markdown'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
