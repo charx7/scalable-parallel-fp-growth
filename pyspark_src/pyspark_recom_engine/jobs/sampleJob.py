36c1f4a2a719f82db0291ae925a5a66b7a24df68
charx <carlos.huertaso@udlap.mx>
2019-02-19 21:57:29 +0100

    spark submit works with interpackage dependancies

--- pyspark_src/pyspark_recom_engine/jobs/sampleJob.py
+++ pyspark_src/pyspark_recom_engine/jobs/sampleJob.py
@@ -1,16 +1,16 @@
-#from pyspark_recom_engine.spark import get_spark
-from pyspark.sql import SparkSession
+from pyspark_recom_engine.spark import get_spark
+#from pyspark.sql import SparkSession
 
 def main():
     '''
         Sample Spark job to test if everything is ok!
     '''
-    spark = (SparkSession.builder
-                .master("local")
-                .appName("recom-engine")
-                .getOrCreate())
+    # spark = (SparkSession.builder
+    #             .master("local")
+    #             .appName("recom-engine")
+    #             .getOrCreate())
 
-    #spark = get_spark()
+    spark = get_spark()
     sc = spark.sparkContext
     rdd = sc.parallelize([1,2,3,4,5,6,7])
     
